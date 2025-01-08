b6ccc31c282b062c34a72261c2523e37944cfaf2
José Duarte <jduartea@gmail.com>
2025-01-08 12:37:51 -0300

    refactor: add exports in admoai.dart

--- lib/admoai.dart
+++ lib/admoai.dart
@@ -3,3 +3,5 @@ export 'src/configs.dart';
 export 'src/models/decision_request.dart';
 export 'src/models/decision_response.dart';
 export 'src/models/decision_request_builder.dart';
+export 'src/utils/app_details.dart';
+export 'src/utils/device_details.dart';
